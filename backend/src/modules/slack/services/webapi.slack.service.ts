9700939869f58c75538e44edd0d8295a321661cd
mourabraz <cmourabraz@gmail.com>
2022-05-12 23:07:45 +0100

    chore: rename class member

--- backend/src/modules/slack/services/webapi.slack.service.ts
+++ backend/src/modules/slack/services/webapi.slack.service.ts
@@ -4,9 +4,9 @@ import { WebClient } from '@slack/web-api';
 import { SLACK_API_BOT_TOKEN } from 'src/libs/constants/slack';
 
 export class WebApiSlackService {
-  public webApi: WebClient;
+  public client: WebClient;
 
   constructor(private readonly configService: ConfigService) {
-    this.webApi = new WebClient(this.configService.get(SLACK_API_BOT_TOKEN));
+    this.client = new WebClient(this.configService.get(SLACK_API_BOT_TOKEN));
   }
 }
