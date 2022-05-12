b8da1665d3116dfed79a7ba82623fbfc5dadc553
mourabraz <cmourabraz@gmail.com>
2022-05-12 23:07:13 +0100

    chore: change return type functions on conversations service

--- backend/src/modules/slack/interfaces/services/conversations.slack.service.ts
+++ backend/src/modules/slack/interfaces/services/conversations.slack.service.ts
@@ -1,9 +1,9 @@
 import { CreateChannelDto } from '../../dto/create.channel.slack.dto';
 
 export interface ConversationsSlackServiceInterface {
-  createChannel(createChannelDto: CreateChannelDto): Promise<void>;
+  createChannel(createChannelDto: CreateChannelDto): Promise<string>;
 
-  inviteUsersToChannel(channelId: string, usersIds: string[]): Promise<void>;
+  inviteUsersToChannel(channelId: string, usersIds: string[]): Promise<boolean>;
 
-  fetchMembersFromChannel(channelId: string): Promise<string[]>;
+  fetchMembersFromChannelSlowly(channelId: string): Promise<string[]>;
 }
