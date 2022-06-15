228c69a64e59a6ca780d59f6ee99edbd061d0951
Daniel Sousa <d.sousa@kigroup.de>
2022-06-15 07:08:27 +0100

    feat: add type to use on useForm hook

---
+++ frontend/src/types/board/SchedulingForm.d.ts
@@ -0,0 +1,10 @@
+type SchedulingFormType = {
+	form: { label: string; value: string };
+	to: { label: string; value: string };
+	timeRange?: { label: string; value: string };
+	timeUnit?: { label: string; value: string };
+	reminderTimeRange?: { label: string; value: string };
+	reminderTimeUnit?: { label: string; value: string };
+};
+
+export { SchedulingFormType };
